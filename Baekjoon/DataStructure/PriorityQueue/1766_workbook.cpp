661cdc91d8c95e4f96fecd27cd093ee656e8c98c
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-01-13 01:06:36 +0900

    21/01/13 solved 1766_workbook

--- Baekjoon/DataStructure/PriorityQueue/1766_workbook.cpp
+++ Baekjoon/DataStructure/PriorityQueue/1766_workbook.cpp
@@ -2,47 +2,39 @@
 #include <string>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
-#define max 32000
+#define max 32010
 vector <int> exist;
 
-bool isExist(int a) {
-	for (int i = 0; i < exist.size(); i++) {
-		if (exist[i] == a)
-			return false;
-	}
-	return true;
-}
 int main() {
 	int n, m;
 	cin >> n >> m;
 	priority_queue< int, vector<int>, greater<int> > pq;
 	vector <int> num[max];
-	
+	vector <int> pr(max,0);
+
 	int a, b;
 	for (int i = 0; i < m; i++) {
 		cin >> a >> b;
-		pq.push(b);
-		num[b].push_back(a);
-		exist.push_back(a);
+		num[a].push_back(b);
+		pr[b]++;
 	}
 
 	for (int i = 1; i <= n; i++) {
-		if (num[i].size() == 0 &&isExist(i))
+		if (pr[i] == 0 )
 			pq.push(i);
 	}
 
-	int size = pq.size();
-	int index;
-	for(int j=0; j<size; j++){
-		if (j > 0)
-			cout << " ";
+	int index, p;
+	while(!pq.empty()){
 		index = pq.top();
 		pq.pop();
-		
+		cout << index<<" ";
 		for (int i = 0; i < num[index].size(); i++) {
-			cout << num[index][i]<<" ";
+			p= --pr[num[index][i]];
+			if (p == 0)
+				pq.push(num[index][i]);
 		}
-		cout << index;
 	}
 }
