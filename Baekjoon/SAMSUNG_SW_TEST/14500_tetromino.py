4c9dbafd5523bac16d0c7b57ed74dc370010be2f
sua1223 <70890638+sua1223@users.noreply.github.com>
2022-04-26 23:33:08 +0900

    22/04/26 solved 14500_tetromino

--- Baekjoon/SAMSUNG_SW_TEST/14500_tetromino.py
+++ Baekjoon/SAMSUNG_SW_TEST/14500_tetromino.py
@@ -1,4 +1,5 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
@@ -10,38 +11,54 @@ for _ in range(N):
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
+visited = [[0]*M for _ in range(N)]
 max_sum = 0
+
+def dfs(i, j, dsum, cnt):
+    global max_sum
+    if cnt == 4:
+        if dsum > max_sum:
+            max_sum = dsum
+        return
+
+    for k in range(4):
+        ni = i + dx[k]
+        nj = j + dy[k]
+        if ni < 0 or nj < 0 or ni >= N or nj >= M:
+            continue
+        if visited[ni][nj] == 0:
+            visited[ni][nj] = 1
+            dfs(ni, nj, dsum+arr[ni][nj], cnt+1)
+            visited[ni][nj] = 0
+
+
+# ㅏ, ㅓ, ㅗ, ㅜ
+def exception(i, j):
+    global max_sum
+    for p in range(4):
+        dsum = arr[i][j]
+        for k in range(4):
+            if p == k:
+                continue
+            if i + dx[k] < 0 or j + dy[k] < 0 or i + dx[k] >= N or j + dy[k] >= M:
+                dsum = 0
+                break
+            dsum += arr[i + dx[k]][j + dy[k]]
+
+        if max_sum < dsum:
+            max_sum = dsum
+
+
 for i in range(N):
     for j in range(M):
-        q = deque()
-        q.append([i, j])
-        cnt = 1
-        sum = arr[i][j]
-        visited = []
-        visited.append([i, j])
-        while q:
-            x, y = q.popleft()
-            for k in range(4):
-                if x + dx[k] < 0 or y + dy[k] < 0 or x + dx[k] >= N or y + dy[k] >= M:
-                    continue
-                loc = [x + dx[k], y + dy[k]]
-                if loc in visited:
-                    continue
-                x += dx[k]
-                y += dy[k]
-                cnt += 1
-                sum += arr[x][y]
-                visited.append([x, y])
-                q.append([x, y])
-                if cnt == 4:
-                    break
-            if cnt == 4:
-                break
-        if max_sum < sum:
-            max_sum = sum
-            print(sum, visited)
+        exception(i, j)
+        visited[i][j] = 1
+        dfs(i, j, arr[i][j], 1)
+        visited[i][j] = 0
 
 print(max_sum)
+
+
 
 
 
